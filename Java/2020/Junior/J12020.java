df0f8ad27836646b3a7ffb6b54c635a12dbe658c
Bhuvan Teja Kudupudi <69252541+BhuvanK123@users.noreply.github.com>
2021-12-15 19:48:43 -0500

    Add files via upload

---
+++ Java/2020/Junior/J12020.java
@@ -0,0 +1,29 @@
+package Junior;
+
+import java.util.*;
+
+public class J12020 {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	    // Scanner m = new Scanner(System.in);
+	    // Scanner l = new Scanner(System.in);
+	    
+	    int Sval =  s.nextInt();
+	    int Mval = s.nextInt();
+	    int Lval = s.nextInt();
+	    
+	    int total = Sval*1 + Mval*2 + Lval*3; 
+	
+	    if (total >= 10) {
+	      System.out.println("happy");
+	    }
+	    else if (total < 10) {
+	      System.out.println("sad");
+	    }
+	    
+	    s.close();
+	    // m.close();
+	    // l.close();
+	}
+}
